d2fcfa9d86e01226945f3054f73bf6c3871f72e6
Oleg Ketrar <oleg.ketrar@icloud.com>
2016-11-27 16:45:09 +0200

    initial commit

---
+++ Sources/NumberPad.h
@@ -0,0 +1,19 @@
+//
+//  NumberPad iOS.h
+//  NumberPad iOS
+//
+//  Created by Oleg Ketrar on 27.11.16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NumberPad iOS.
+FOUNDATION_EXPORT double NumberPadVersionNumber;
+
+//! Project version string for NumberPad iOS.
+FOUNDATION_EXPORT const unsigned char NumberPadVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NumberPad_iOS/PublicHeader.h>
+
+
